8d3d0591a76760b5d170a33dd376a9c044f17fcc
Bipin C <63953352+Bunty9@users.noreply.github.com>
2024-12-30 02:29:23 +0530

    hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,9 +2,11 @@ import ArrowRight from "@/assets/arrow-right.svg";
 import Logo from "@/assets/logosaas.png";
 import Image from "next/image";
 import MenuIcon from "@/assets/menu.svg";
+
+
 export const Header = () => {
 	return (
-		<header>
+		<header className="sticky top-0 backdrop-blur-sm z-10">
 			<div className="flex justify-center items-center bg-black text-white text-sm py-3 gap-3">
 				<p className="text-white/60 hidden md:block">
 					Streamline workflow and boot productivity.
@@ -19,15 +21,13 @@ export const Header = () => {
 					<div className="flex justify-between items-center">
 						<Image src={Logo} alt="" height={40} width={40} />
 						<MenuIcon className="h-5 w-5 md:hidden" />
-						<nav className="md:flex gap-6 text-black/60 items-center">
+						<nav className="hidden md:flex gap-6 text-black/60 items-center">
 							<a href="#">About</a>
 							<a href="#">Features</a>
 							<a href="#">Customers</a>
 							<a href="#">Updates</a>
 							<a href="#">Help</a>
-							<button className="bg-black text-white py-2 px-4 rounded-lg font-medium inline-flex align-items justify-center tracking-tighter">
-								Get for Free
-							</button>
+							<button className="btn btn-primary">Get for Free</button>
 						</nav>
 					</div>
 				</div>
