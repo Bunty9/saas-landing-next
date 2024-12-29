1c9969e588ae765c2773cd218d33056bf8deac62
Bipin C <63953352+Bunty9@users.noreply.github.com>
2024-12-30 04:45:35 +0530

    showcase

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,26 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import apexLogo from "@/assets/logo-apex.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+	return (
+		<div className="py-8 md:py-12 bg-white">
+			<div className="container">
+				<div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+					<div className="flex gap-10 flex-none">
+						<Image src={acmeLogo} alt="" className="logo-ticker" />
+						<Image src={quantumLogo} alt="" className="logo-ticker" />
+						<Image src={echoLogo} alt="" className="logo-ticker" />
+						<Image src={celestialLogo} alt="" className="logo-ticker" />
+						<Image src={apexLogo} alt="" className="logo-ticker" />
+						<Image src={pulseLogo} alt="" className="logo-ticker" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,42 @@
+import productImage from "@/assets/product-image.png";
+import Image from "next/image";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+
 export const ProductShowcase = () => {
-  return null;
+	return (
+		<section className="bg-gradient-to-b from -[#FFF] to-[#D2DCFF] py-24 overflow-x-clip">
+			<div className="container">
+				<div className="max-w-[540px] mx-auto">
+					<div className="flex justify-center">
+						<div className="tag">Boost your Productivity</div>
+					</div>
+					<h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#273d88] text-transparent bg-clip-text mt-4">
+						A more effective way to track your Progress
+					</h2>
+					<p className="text-center text-[18px] leading-[20px] tracking-tighter text-[#191c3b] mt-4">
+						Effortlessly turn your ideas into a fully functional, responsive,
+						SaaS website in just minutes.
+					</p>
+				</div>
+				<div className="relative">
+					<Image src={productImage} alt="" className="mt-4" />
+					<Image
+						src={pyramidImage}
+						alt=""
+						className="hidden md:block absolute -right-36 -top-24 "
+						height={200}
+						width={200}
+					/>
+					<Image
+						src={tubeImage}
+						alt=""
+						className="hidden md:block absolute -left-36 bottom-2 "
+						height={200}
+						width={200}
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
