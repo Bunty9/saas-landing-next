56b483e98143b2cfc05a2250aa95fdf3034d8a90
Bipin C <63953352+Bunty9@users.noreply.github.com>
2024-12-30 05:30:27 +0530

    pricing

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -5,16 +5,16 @@ import tubeImage from "@/assets/tube.png";
 
 export const ProductShowcase = () => {
 	return (
-		<section className="bg-gradient-to-b from -[#FFF] to-[#D2DCFF] py-24 overflow-x-clip">
+		<section className="bg-gradient-to-b from-[#FFF] to-[#a6b8fc] py-24 overflow-x-clip">
 			<div className="container">
-				<div className="max-w-[540px] mx-auto">
+				<div className="section-heading">
 					<div className="flex justify-center">
 						<div className="tag">Boost your Productivity</div>
 					</div>
-					<h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#273d88] text-transparent bg-clip-text mt-4">
+					<h2 className="section-title">
 						A more effective way to track your Progress
 					</h2>
-					<p className="text-center text-[18px] leading-[20px] tracking-tighter text-[#191c3b] mt-4">
+					<p className="section-ptag">
 						Effortlessly turn your ideas into a fully functional, responsive,
 						SaaS website in just minutes.
 					</p>
